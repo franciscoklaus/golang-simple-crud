eb2e8347edf6b4c2062da70f2e633c861bdd0fa1
Francisco Klaus <franciscoklausfilho@hotmail.com>
2024-01-05 14:34:22 -0300

    route delete created

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios/{id}", servidor.DeletarUsuario).Methods(http.MethodDelete)
 
 	fmt.Println("Escutando na porta 5000...")
 	log.Fatal(http.ListenAndServe(":5000", router))
